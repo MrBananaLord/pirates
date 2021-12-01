71486a38719031bbbf3e0e97c0259d0698fd9f8d
Jan Bator <jan.bator@gmail.com>
2021-12-01 14:33:39 +0100

    Simplify physics.

--- Player.gd
+++ Player.gd
@@ -15,39 +15,14 @@ func _physics_process(delta):
 		$Ship.rudderTurnStarboard()
 	if Input.is_action_pressed("rudder_starboard_1"):
 		$Ship.rudderTurnPortside()
-	if Input.is_action_pressed("sail_clockwise_1"):
-		$Ship.sailClockwise()
-	if Input.is_action_pressed("sail_counterclockwise_1"):
-		$Ship.sailCounterclockwise()
 
-#	var sailAngle = $Ship/Hull/Mast.global_transform.basis.z.angle_to(Wind.force)
-#	var strength = abs(sin(sailAngle)) * Wind.strength * $Ship.sailFold
-
-	var direction = transform.basis.z.normalized()
-	var sailForce = Wind.force * $Ship.sailFold / 10
+	var hullAngle = $Ship/Hull.global_transform.basis.z.angle_to(Wind.force)
+	var strength = abs(cos(hullAngle / 2)) * Wind.strength * $Ship.sailFold / 100
+	var direction = transform.basis.z.normalized().rotated(transform.basis.y.normalized(), $Ship.rudderAngle * delta)
+	var velocity = direction * strength
 	
-	Debugger.draw("wind", transform.origin + Vector3(0,5,0), Wind.force, Color(255,0,0))
-
-	var angle = $Ship/Hull/Mast.global_transform.basis.z.angle_to(Wind.force)
-	var dir = Vector3.ZERO.direction_to($Ship/Hull/Mast.global_transform.basis.z.rotated(Vector3.UP, PI / 2))
-	
-	if dir.dot(Wind.force) < 0:
-		dir = dir.rotated(Vector3.UP, PI)
+	Debugger.draw("wind", transform.origin + Vector3(0,0,0), Wind.force, Color(255,0,0))
+	Debugger.draw("velocity", transform.origin + Vector3(0,0,0), velocity)
 		
-#	Debugger.draw("dir", transform.origin + Vector3(0,5,0), dir, Color(0,0,255))
-	var sail = dir.normalized() * $Ship.sailFold / 10 * sin(angle)
-	
-	Debugger.draw("sail2", transform.origin + Vector3(0,5,0), sail, Color(0,0,255))
-	
-#	var keel = direction.normalized().rotated(Vector3.UP, PI / 3) * sail.length()
-	var keel = ((velocity + Wind.force) * -direction).bounce(direction)
-	var newVelocity = Vector3.ZERO.direction_to(direction) * sail.length() + Wind.force
-#	newVelocity.y -= 1000 * delta
-	velocity = newVelocity
-	
-#	Debugger.draw("direction", transform.origin + Vector3(0,5,0), Vector3.ZERO.direction_to(direction) * 10, Color(255, 0, 255))
-	Debugger.draw("velocity", transform.origin + Vector3(0,5,0), newVelocity)
-	Debugger.draw("keel", transform.origin + Vector3(0,5,0), keel, Color(0,255,255))
-
-	rotate_y(- $Ship.rudderAngle / 50)
-	move_and_slide(velocity.rotated(transform.basis.y.normalized(), $Ship.rudderAngle * delta), Vector3.UP)
+	rotate_y(- $Ship.rudderAngle * delta)
+	move_and_slide(velocity, Vector3.UP)
