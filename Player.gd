a9838fadb73e3d313a0f54b083dd040b60eda8b1
Jan Bator <jan.bator@gmail.com>
2021-12-02 09:56:24 +0100

    Cannonade.

--- Player.gd
+++ Player.gd
@@ -6,7 +6,7 @@ var playerId = 0
 var velocity = Vector3.ZERO
 
 func _ready():
-	pass
+	global_transform.origin.y += 10
 
 func _physics_process(delta):
 	if Input.is_action_pressed("sail_up_%s" % playerId):
@@ -26,30 +26,35 @@ func _physics_process(delta):
 	var strength = abs(cos(hull_angle / 2)) * Wind.force.length() * $Ship.sail_fold / 100
 	var direction = transform.basis.z.normalized().rotated(transform.basis.y.normalized(), $Ship.rudder_angle * delta)
 	var velocity = direction * strength
+	velocity.y -= 100 * delta
 	
 	Debugger.draw("wind", transform.origin + Vector3(0,0,0), Wind.force, Color(255,0,0))
 	Debugger.draw("velocity", transform.origin + Vector3(0,0,0), velocity)
 		
 	rotate_y(- $Ship.rudder_angle * delta)
-	move_and_slide(velocity, Vector3.UP)
+	move_and_slide(velocity, direction)
+#	add_torque($Ship.rudder_angle * velocity.rotated(Vector3.FORWARD, PI / 2) * 10)
+#	add_force(velocity, transform.origin)
 
 func _fire_starboard():
-	var ball = Cannonball.instance()
-	
-	add_child(ball)
-	ball.set_as_toplevel(true)
-	
-	ball.transform = global_transform
-	ball.transform.origin += Vector3.UP * 2
-	var direction = transform.basis.z.normalized().rotated(transform.basis.y.normalized(), - PI / 2)
-	ball.apply_central_impulse(direction * 25 + velocity)
+	_fire(- PI / 2, -2)
+	_fire(- PI / 2, 0)
+	_fire(- PI / 2, 2)
 	
 func _fire_portside():
+	_fire(PI / 2, -2)
+	_fire(PI / 2, 0)
+	_fire(PI / 2, 2)
+
+func _fire(angle, offset):
 	var ball = Cannonball.instance()
 	
 	add_child(ball)
 	ball.set_as_toplevel(true)
 	
 	ball.transform.origin += Vector3.UP * 2
-	var direction = transform.basis.z.normalized().rotated(transform.basis.y.normalized(), PI / 2)
-	ball.apply_central_impulse(direction * 25 + velocity)
+	ball.transform.origin += transform.basis.z.normalized() * offset
+	var direction = transform.basis.z.normalized().rotated(transform.basis.y.normalized(), angle).rotated(transform.basis.z.normalized(), angle / 5)
+#	direction.x *= 20
+#	direction.y += 200
+	ball.apply_central_impulse(direction * 2500)
